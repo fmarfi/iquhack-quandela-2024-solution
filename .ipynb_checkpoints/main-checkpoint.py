d5de1d40dccde289f4a778c91294e2356d328722
Batu Yalcin <batuyalcin@Batus-MacBook-Pro.local>
2024-02-04 17:51:25 +0300

    Final Solution

--- .ipynb_checkpoints/main-checkpoint.py
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -3,24 +3,22 @@ import perceval.components as comp
 import numpy as np
 
 def get_CCZ():
-    T = pcvl.PS(np.pi/4)
-    Tdag = pcvl.PS(-np.pi/4)
-    H = pcvl.BS.H()
-    cnot = pcvl.catalog["heralded cnot"].build_processor()
     p = pcvl.Processor("SLOS",6)
-    p.add([4,5],H)
-    p.add([2,3,4,5],cnot)
-    p.add([5],Tdag)
-    p.add([0,1,4,5],cnot)
-    p.add([5],T)
-    p.add([2,3,4,5],cnot)
-    p.add([5],Tdag)
-    p.add([0,1,4,5],cnot)
-    p.add([5],T)
-    p.add([3],T)
-    p.add([4,5],H)
-    p.add([0,1,2,3],cnot)
-    p.add([1],T)
-    p.add([3],Tdag)
-    p.add([0,1,2,3],cnot)
+    #Upper Modes 0,2,4
+
+    p.add([0,1],pcvl.BS.Ry(pcvl.BS.r_to_theta(R1)))
+    p.add([2,3],pcvl.BS.Ry(pcvl.BS.r_to_theta(R1)))
+    p.add([4,5],pcvl.BS.Ry(pcvl.BS.r_to_theta(R1)))
+
+    p.add([0,1],pcvl.BS.Ry(-pcvl.BS.r_to_theta(R3)))
+    p.add([2,3],pcvl.BS.Ry(-pcvl.BS.r_to_theta(R3)))
+    p.add([4,5],pcvl.BS.Ry(-pcvl.BS.r_to_theta(R3)))
+
+    p.add([1,2],pcvl.BS.Ry(pcvl.BS.r_to_theta(R1)))
+    p.add([3,4],pcvl.BS.Ry(pcvl.BS.r_to_theta(R1)))
+    p.add([5],pcvl.PS(np.pi))
+    p.add([0,5],pcvl.BS.Ry(pcvl.BS.r_to_theta(R1)))
+    p.add([1,2,3,4,5],pcvl.PERM([4,0,1,2,3]))
+
+    p.set_postselection(pcvl.PostSelect("[0,1]==1 & [2,3]==1 & [4,5]==1"))
     return p
--- main.py
+++ main.py
@@ -3,24 +3,29 @@ import perceval.components as comp
 import numpy as np
 
 def get_CCZ():
-    T = pcvl.PS(np.pi/4)
-    Tdag = pcvl.PS(-np.pi/4)
-    H = pcvl.BS.H()
-    cnot = pcvl.catalog["heralded cnot"].build_processor()
+    
+    T1 = 1/(1+2**(1/3))
+    R1 = np.sqrt(1-T1**2)
+    T3 = T1**2
+    R3 = np.sqrt(1-T3**2)
     p = pcvl.Processor("SLOS",6)
-    p.add([4,5],H)
-    p.add([2,3,4,5],cnot)
-    p.add([5],Tdag)
-    p.add([0,1,4,5],cnot)
-    p.add([5],T)
-    p.add([2,3,4,5],cnot)
-    p.add([5],Tdag)
-    p.add([0,1,4,5],cnot)
-    p.add([5],T)
-    p.add([3],T)
-    p.add([4,5],H)
-    p.add([0,1,2,3],cnot)
-    p.add([1],T)
-    p.add([3],Tdag)
-    p.add([0,1,2,3],cnot)
+    #Upper Modes 0,2,4
+    #p.add(4,pcvl.BS.H())
+    p.add([0,1],pcvl.BS.Ry(pcvl.BS.r_to_theta(R1)))
+    p.add([2,3],pcvl.BS.Ry(pcvl.BS.r_to_theta(R1)))
+    p.add([4,5],pcvl.BS.Ry(pcvl.BS.r_to_theta(R1)))
+
+    p.add([0,1],pcvl.BS.Ry(-pcvl.BS.r_to_theta(R3)))
+    p.add([2,3],pcvl.BS.Ry(-pcvl.BS.r_to_theta(R3)))
+    p.add([4,5],pcvl.BS.Ry(-pcvl.BS.r_to_theta(R3)))
+
+    p.add([1,2],pcvl.BS.Ry(pcvl.BS.r_to_theta(R1)))
+    p.add([3,4],pcvl.BS.Ry(pcvl.BS.r_to_theta(R1)))
+    p.add([5],pcvl.PS(np.pi))
+    p.add([0,5],pcvl.BS.Ry(pcvl.BS.r_to_theta(R1)))
+    p.add([1,2,3,4,5],pcvl.PERM([4,0,1,2,3]))
+    
+    p.add([5],pcvl.PS(np.pi))
+
+    p.set_postselection(pcvl.PostSelect("[0,1]==1 & [2,3]==1 & [4,5]==1"))
     return p
